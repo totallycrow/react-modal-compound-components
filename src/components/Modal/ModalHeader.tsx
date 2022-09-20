8e1f754a7d9dc6697daf7041215a07604160f294
Adrian Gajda <adrian.gajda@hotmail.com>
2022-09-20 16:20:52 +0100

    updates

--- src/components/Modal/ModalHeader.tsx
+++ src/components/Modal/ModalHeader.tsx
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 
-export default class ModalHeader extends Component<any, any> {
+type HeaderProps = {
+  title: string;
+  toggle?: Function;
+};
+
+export default class ModalHeader extends Component<HeaderProps> {
   render() {
-    // @ts-ignore
     return (
       <div>
         <h2>{this.props.title}</h2>
-        <button onClick={this.props.toggle}>Click to close</button>
+        <button
+          onClick={() => {
+            if (this.props.toggle) {
+              this.props.toggle();
+            }
+          }}
+        >
+          Click to close
+        </button>
+        {/* works */}
+        {/* @ts-ignore */}
+        {/* <button onClick={this.props.toggle}>Click to close</button> */}
       </div>
     );
   }
